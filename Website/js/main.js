6138fb07912d7a514f574e8a3d25048927f0e7ef
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2017-11-04 05:05:39 +0100

    Added basic includes to jquery, css, javascript files

---
+++ Website/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  console.log("Hello world from the console");
+
+  $.get("doubledart.com/", function(data) {
+    console.log("Get request data: ");
+    console.log(data);
+  });
+
+  $.get("html/testpartial.html", function(data) {
+    console.log("Get request data: ");
+    console.log(data);
+  })
+});
